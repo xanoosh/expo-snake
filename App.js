3078954fe7fbb561a45f8890e4bd25adeb1ae4a2
xanoosh <xanoosh@gmail.com>
2021-08-28 19:24:51 +0200

    trying to start game loop - problem with snakeposition render in sankeboard component

--- App.js
+++ App.js
@@ -6,33 +6,67 @@ import { mainStyle } from './styles/Style';
 import SnakeBoard from './components/SnakeBoard';
 
 export default function App() {
-  const boardSquareSize = 20;
-  const [snakePosition, setSnakePosition] = useState([]);
+  const boardSquareSize = 10;
+  const [snakePosition, setSnakePosition] = useState([
+    {
+      x: Math.round(boardSquareSize / 2),
+      y: Math.round(boardSquareSize / 2) - 1,
+    },
+    {
+      x: Math.round(boardSquareSize / 2),
+      y: Math.round(boardSquareSize / 2),
+    },
+    {
+      x: Math.round(boardSquareSize / 2),
+      y: Math.round(boardSquareSize / 2) + 1,
+    },
+  ]);
   // gonna need below states when some kind of loop will be implemented
   //(preferrably requestAnimationFrame)
-  // const [snakeDirection, setSnakeDirection] = useState('left');
-  // const [snakeSpeed, setSnakeSpeed] = useState(1);
+  const [snakeDirection, setSnakeDirection] = useState('left');
+  const [snakeSpeed, setSnakeSpeed] = useState(3); //3 moves per second
   //food state:
   // const [snakeFoodPosition, setSnakeFoodPosition] = useState({ x: 10, y: 2 });
   // const [snakeAteFood, setSnakeAteFood] = useState(false);
-  //calcualte starting snake position on mount:
+
+  //check time in miliseconds:
+  let lastTimeStamp = 0;
+  const updateFrame = (timeStamp) => {
+    if (timeStamp - lastTimeStamp >= 1000 / snakeSpeed) {
+      lastTimeStamp = timeStamp;
+      return true;
+    }
+    return false;
+  };
+  const gameLoop = (timeStamp) => {
+    if (updateFrame(timeStamp)) {
+      move();
+    }
+    //some condition for end
+    requestAnimationFrame(gameLoop);
+  };
+
+  //calculate starting snake position on mount:
   useEffect(() => {
-    setSnakePosition(() => {
-      return [
-        {
-          x: Math.round(boardSquareSize / 2),
-          y: Math.round(boardSquareSize / 2) - 1,
-        },
-        {
-          x: Math.round(boardSquareSize / 2),
-          y: Math.round(boardSquareSize / 2),
-        },
-        {
-          x: Math.round(boardSquareSize / 2),
-          y: Math.round(boardSquareSize / 2) + 1,
-        },
-      ];
-    });
+    // (async () => {
+    //   await setSnakePosition(() => {
+    //     return [
+    //       {
+    //         x: Math.round(boardSquareSize / 2),
+    //         y: Math.round(boardSquareSize / 2) - 1,
+    //       },
+    //       {
+    //         x: Math.round(boardSquareSize / 2),
+    //         y: Math.round(boardSquareSize / 2),
+    //       },
+    //       {
+    //         x: Math.round(boardSquareSize / 2),
+    //         y: Math.round(boardSquareSize / 2) + 1,
+    //       },
+    //     ];
+    //   }).then(requestAnimationFrame(gameLoop));
+    //   //start loop?
+    // })();
   }, []);
   //move snake function (update snake position):
   const move = (snakeDirection) => {
@@ -60,13 +94,14 @@ export default function App() {
   };
   return (
     <View style={mainStyle.container}>
-      <Text style={{ fontWeight: 'bold', fontSize: 20, marginBottom: 20 }}>
-        Snake board below:
-      </Text>
-      <SnakeBoard
-        boardSquareSize={boardSquareSize}
-        getSnakePosition={snakePosition}
-      />
+      {snakePosition.length !== 0 ? (
+        <SnakeBoard
+          boardSquareSize={boardSquareSize}
+          getSnakePosition={snakePosition}
+        />
+      ) : (
+        <Text>Loading..</Text>
+      )}
       <View style={mainStyle.buttonGroup}>
         <Pressable
           style={[mainStyle.buttonUp, mainStyle.button]}
@@ -91,6 +126,12 @@ export default function App() {
           onPress={() => move('right')}
         >
           <View style={mainStyle.arrowRight}></View>
+        </Pressable>
+        <Pressable
+          style={[mainStyle.buttonPlayPause]}
+          onPress={() => requestAnimationFrame(gameLoop)}
+        >
+          <Text style={mainStyle.buttonPlayPauseText}>Play</Text>
         </Pressable>
       </View>
 
